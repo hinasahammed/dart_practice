069d7b8a84070a2989a8a2c92b49616358a1834b
Fathima huda <82311707+hinasahammed@users.noreply.github.com>
2024-06-05 11:42:39 +0530

    ..

---
+++ lib/dartPractice/guess_number.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  var getingRandomNumber = Random().nextInt(100) + 1;
+  print("Guess the number from 1 to 100");
+  int userNumber = int.parse(stdin.readLineSync()!);
+
+  if (userNumber < getingRandomNumber) {
+    print("too low");
+  } else if (userNumber > getingRandomNumber) {
+    print("Too High");
+  } else if (userNumber == getingRandomNumber) {
+    print("You guessed right");
+  } else {
+    print("Check your number");
+  }
+}
