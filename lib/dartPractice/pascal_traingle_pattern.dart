0cd64977880b8011df58b4faea1df8ec7389c9e3
Fathima huda <82311707+hinasahammed@users.noreply.github.com>
2024-06-26 11:41:41 +0530

    pascal traingle

--- lib/dartPractice/pascal_traingle_pattern.dart
+++ lib/dartPractice/pascal_traingle_pattern.dart
@@ -1,15 +1,19 @@
 import 'dart:io';
 
 void main() {
-  for (var i = 0; i < 5; i++) {
-   for (var s = 0; s < 5-i; s++) {
-     
-    stdout.write(" ");
-   }
-   for (var j = 0; j < i+1; j++) {
-     
-    stdout.write("1 ");
-   }
-    stdout.write("\n");
+  print("Enter row:");
+  int n = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < n; i++) {
+    int number = 1;
+    for (var s = 0; s < n - i; s++) {
+      stdout.write(" ");
+    }
+    for (int j = 0; j <= i; j++) {
+      stdout.write('$number');
+
+      number = number * (i - j) ~/ (j + 1);
+    }
+    stdout.writeln();
   }
 }
