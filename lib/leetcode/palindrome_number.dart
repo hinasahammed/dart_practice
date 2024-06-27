1cf6df5d3fe5827377f1f10f90388865455de01a
Fathima huda <82311707+hinasahammed@users.noreply.github.com>
2024-06-27 19:37:54 +0530

    created leetcode

---
+++ lib/leetcode/palindrome_number.dart
@@ -0,0 +1,10 @@
+void main() {
+print(  isPalindrome(121));
+}
+
+bool isPalindrome(int x) {
+  var usvalue = x.toString();
+  var newValue = int.parse(usvalue.split('').reversed.join());
+  var isCheck = x == newValue;
+  return isCheck;
+}
