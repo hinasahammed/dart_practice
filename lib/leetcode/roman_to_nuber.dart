3e9714c11ef7f578acee8b6a786acba13c38f238
Fathima huda <82311707+hinasahammed@users.noreply.github.com>
2024-06-30 18:23:37 +0530

    added simple method for roman numbers

--- lib/leetcode/roman_to_nuber.dart
+++ lib/leetcode/roman_to_nuber.dart
@@ -112,4 +112,27 @@ void main() {
     }
   }
   print(total);
+
+  //simple method
+  // int sum = 0;
+  //   int prevValue = 0;
+  //   Map<String, int> value = {
+  //     'I': 1,
+  //     'V': 5,
+  //     'X': 10,
+  //     'L': 50,
+  //     'C': 100,
+  //     'D': 500,
+  //     'M': 1000,
+  //   };
+
+  //   for (int i = 0; i < s.length; i++) {
+  //     int currentValue = value[s[i]]!;
+  //     sum += (currentValue > prevValue)
+  //         ? (currentValue - 2 * prevValue)
+  //         : currentValue;
+  //     prevValue = currentValue;
+  //   }
+
+  //   return sum;
 }
