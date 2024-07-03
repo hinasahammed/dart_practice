a766e9e495e5c7bbd8194959bc5cf9037165ff84
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-07-03 10:53:00 +0530

    Update longest_common_prefix.dart

--- lib/leetcode/longest_common_prefix.dart
+++ lib/leetcode/longest_common_prefix.dart
@@ -1,10 +1,17 @@
 void main() {
   List<String> strs = ["flower", "flow", "flight"];
-  List<String> newVal = [];
+  List<String> newValList = [];
+  List<String> commonList = [];
   for (var i = 0; i < strs.length; i++) {
-    newVal.add(strs[i]);
-    for (var j = 0; j < strs.length; j++) {
-      
+    for (var j = 0; j < strs[i].length; j++) {
+      if (i == 0) {
+        newValList.add(strs[i][j]);
+      }
+      print(newValList);
+      if (strs[i] == newValList[i]) {
+        commonList.add(newValList[i]);
+      }
     }
   }
+  print(commonList);
 }
