c0823ab48cf62ed8d8ee0c49d4c6571689591470
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-07-05 09:50:36 +0530

    tried some new technique

--- lib/leetcode/intersection_of_two_arrays.dart
+++ lib/leetcode/intersection_of_two_arrays.dart
@@ -1,9 +1,17 @@
 void main() {
-  print(intersect([1, 2, 2, 1], [2, 2]));
+  print(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
 }
 
 List<int> intersect(List<int> nums1, List<int> nums2) {
+  List<int> twoCombined = [];
   var nums1Set = nums1.toSet();
   var nums2Set = nums2.toSet();
-  return nums1Set.intersection(nums2Set).toList();
+  var nums1Intersection = nums1Set.intersection(nums1Set).toList();
+  var nums2Intersection = nums2Set.intersection(nums2Set).toList();
+  var combinedIntersection = nums1Set.intersection(nums2Set).toList();
+  nums1Intersection.addAll(nums2Intersection);
+  twoCombined.addAll(nums1Intersection.where(
+    (element) => combinedIntersection.contains(element),
+  ));
+  return twoCombined;
 }
