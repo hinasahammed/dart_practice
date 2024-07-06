3baadd7bc2100922354267a8edd41f3140897c3b
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-07-06 11:16:33 +0530

    completed leetcode program search insert position

--- lib/leetcode/search_insert_position.dart
+++ lib/leetcode/search_insert_position.dart
@@ -1,17 +1,31 @@
 void main() {
-  searchInsert([1, 3, 5, 6], 2);
+  print(searchInsert([3,6,7,8,10], 5));
 }
 
-void searchInsert(List<int> nums, int target) {
+int searchInsert(List<int> nums, int target) {
+  int realVal = 1;
   if (nums.contains(target)) {
-    print(nums.indexOf(target));
+    realVal = nums.indexOf(target);
   } else {
     for (var i = 0; i < nums.length; i++) {
-      if (i != nums.length - 1) {
-        if (nums[i] > target && nums[i + 1] < target) {
-          print(i);
+      if (nums[i] + 1 == target) {
+        realVal = i + 1;
+        break;
+      } else if (i == 0) {
+        if (target < nums[i]) {
+          realVal = i;
+          break;
+        }
+      } else if (nums[i] - 1 == target) {
+        realVal = i;
+        break;
+      }  else if (i == nums.length - 1) {
+        if (target > nums[i]) {
+          realVal = i + 1;
+          break;
         }
       }
     }
   }
+  return realVal;
 }
