9b17592e1c77dffb432b068a0841e0879a0ad7e9
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-07-06 10:15:49 +0530

    Update search_insert_position.dart

--- lib/leetcode/search_insert_position.dart
+++ lib/leetcode/search_insert_position.dart
@@ -1,5 +1,17 @@
 void main() {
-  searchInsert([1, 3, 5, 6], 5);
+  searchInsert([1, 3, 5, 6], 2);
 }
 
-void searchInsert(List<int> nums, int target) {}
+void searchInsert(List<int> nums, int target) {
+  if (nums.contains(target)) {
+    print(nums.indexOf(target));
+  } else {
+    for (var i = 0; i < nums.length; i++) {
+      if (i != nums.length - 1) {
+        if (nums[i] > target && nums[i + 1] < target) {
+          print(i);
+        }
+      }
+    }
+  }
+}
