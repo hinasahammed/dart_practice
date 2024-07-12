75a7489b2ea14d4d3614ec0af7222de3180b6d35
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-07-12 15:13:56 +0530

    Update first_missing_positive.dart

--- lib/logical test/first_missing_positive.dart
+++ lib/logical test/first_missing_positive.dart
@@ -1,23 +1,16 @@
 void main() {
-  List nums = [3, 4, -1, 1];
+  List nums = [3,4,-1,1];
   nums.sort();
-  // -1,1,3,4
-  var smallest = nums[0];
-  if (smallest - 1 <= 0) {
-    for (var i = 0; i < nums.length; i++) {
-      if (i != nums.length - 1) {
-        if (!(nums[i] == nums[i + 1] - 1)) {
-          print(nums[i + 1] - 1);
-          break;
-        } else {
-          if (i == nums.length - 1) {
-            print("og ${nums.last + 1}");
-            break;
-          }
-        }
-      }
+  nums.removeWhere(
+    (element) => element <= 0,
+  );
+  for (var i = 1; i <= nums.last; i++) {
+    if (!nums.contains(i)) {
+      print(i);
+      break;
+    } else if (i == nums.last) {
+      print(i + 1);
+      break;
     }
-  } else {
-    print(smallest - 1);
   }
 }
